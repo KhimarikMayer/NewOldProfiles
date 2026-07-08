cc775364a51b2effe557504df01b83d87fc595fe
KhimarikMayer <141296494+KhimarikMayer@users.noreply.github.com>
2026-07-08 15:48:17 +0900

    Refactor store exports for better readability

--- src/modules/stores.js
+++ src/modules/stores.js
@@ -1,22 +1,22 @@
 import { Webpack } from 'betterdiscord';
 
-export const AccessibilityStore = betterdiscord.Webpack.getStore("AccessibilityStore");
-export const AuthenticationStore = betterdiscord.Webpack.getStore("AuthenticationStore");
-export const RelationshipStore = betterdiscord.Webpack.getStore("RelationshipStore");
-export const ActivityStore = betterdiscord.Webpack.getStore("PresenceStore");
-export const UserStore = betterdiscord.Webpack.getStore("UserStore");
-export const ChannelStore = betterdiscord.Webpack.getStore("ChannelStore");
-export const NewGameStore = betterdiscord.Webpack.getStore("NewGameStore");
-export const PermissionStore = betterdiscord.Webpack.getStore("PermissionStore");
-export const GuildStore = betterdiscord.Webpack.getStore("GuildStore");
-export const StreamStore = betterdiscord.Webpack.getStore("ApplicationStreamingStore");
-export const UserProfileStore = betterdiscord.Webpack.getStore("UserProfileStore");
-export const ApplicationStore = betterdiscord.Webpack.getStore("ApplicationStore");
-export const ApplicationStreamPreviewStore = betterdiscord.Webpack.getStore("ApplicationStreamPreviewStore");
-export const VoiceStateStore = betterdiscord.Webpack.getStore("VoiceStateStore");
-export const GuildMemberStore = betterdiscord.Webpack.getStore("GuildMemberStore");
-export const GuildRoleStore = betterdiscord.Webpack.getStore("GuildRoleStore");
-export const StreamerModeStore = betterdiscord.Webpack.getStore("StreamerModeStore");
-export const { useStateFromStores } = betterdiscord.Webpack.getMangled((m) => m.Store, {
-          useStateFromStores: betterdiscord.Webpack.Filters.byStrings("useStateFromStores")
+export const AccessibilityStore = /* @__PURE__ */ Webpack.getStore("AccessibilityStore");
+export const AuthenticationStore = /* @__PURE__ */ Webpack.getStore("AuthenticationStore");
+export const RelationshipStore = /* @__PURE__ */ Webpack.getStore("RelationshipStore");
+export const ActivityStore = /* @__PURE__ */ Webpack.getStore("PresenceStore");
+export const UserStore = /* @__PURE__ */ Webpack.getStore("UserStore");
+export const ChannelStore = /* @__PURE__ */ Webpack.getStore("ChannelStore");
+export const NewGameStore = /* @__PURE__ */ Webpack.getStore("NewGameStore");
+export const PermissionStore = /* @__PURE__ */ Webpack.getStore("PermissionStore");
+export const GuildStore = /* @__PURE__ */ Webpack.getStore("GuildStore");
+export const StreamStore = /* @__PURE__ */ Webpack.getStore("ApplicationStreamingStore");
+export const UserProfileStore = /* @__PURE__ */ Webpack.getStore("UserProfileStore");
+export const ApplicationStore = /* @__PURE__ */ Webpack.getStore("ApplicationStore");
+export const ApplicationStreamPreviewStore = /* @__PURE__ */ Webpack.getStore("ApplicationStreamPreviewStore");
+export const VoiceStateStore = /* @__PURE__ */ Webpack.getStore("VoiceStateStore");
+export const GuildMemberStore = /* @__PURE__ */ Webpack.getStore("GuildMemberStore");
+export const GuildRoleStore = /* @__PURE__ */ Webpack.getStore("GuildRoleStore");
+export const StreamerModeStore = /* @__PURE__ */ Webpack.getStore("StreamerModeStore");
+export const { useStateFromStores } = /* @__PURE__ */ Webpack.getMangled(m => m.Store, {
+        useStateFromStores: /* @__PURE__ */ Webpack.Filters.byStrings("useStateFromStores")
         }, { raw: true });
