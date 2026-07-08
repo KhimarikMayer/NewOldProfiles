eb0525ae1494274f06e82d91b8cc66d7464ccd5e
KhimarikMayer <141296494+KhimarikMayer@users.noreply.github.com>
2026-07-08 15:46:13 +0900

    Fix string quotes in locale.js for WIDGETS

--- src/common/locale.js
+++ src/common/locale.js
@@ -6,7 +6,7 @@ export const locale = {
   Sections: {
     MUTUAL_FRIENDS: tabs.FRIENDS,
     MUTUAL_GUILDS: tabs.SERVERS,
-    WIDGETS: betterdiscord.Data.load("boardTab") ? tabs.BOARD : null
+    WIDGETS: Data.load('boardTab') ? tabs.BOARD : null
   },
   Strings: {
     ABOUT_ME: () => getIntlString("61W33d"),
