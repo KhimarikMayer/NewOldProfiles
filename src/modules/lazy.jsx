5138f683bb8574e2330dfa1f22a9efbf61e73416
KhimarikMayer <141296494+KhimarikMayer@users.noreply.github.com>
2026-07-08 15:19:36 +0900

    Refactor components and update exports in lazy.jsx

--- src/modules/lazy.jsx
+++ src/modules/lazy.jsx
@@ -1,118 +1,227 @@
+import React from 'react';
 import { Webpack } from 'betterdiscord';
 import { PopUtils, MessageButtons } from '@modules/common';
+import { intl } from '../modules.js';
+import { ButtonClasses } from '../modules.js';
 
 let MessageButtonLarge;
 let MessageButtonSmall;
-let FriendsButton;
 let MoreOverflowButton;
-let FriendAddButton;
+let SendFriendRequestButton;
 let EditProfileButton;
 let BotAddButton;
 let MarkdownFormat;
 let NoteRenderer;
 let ConnectionRenderer;
-let BotDataRenderer;
 let Board;
 let RolePermissionCheck;
 let TagRenderer;
 let MutualFriends;
 let MutualServers;
 
-function MessageButtonLargeComponent({ autoFocus, onClose, userId }) {
-    MessageButtonLarge ??= Webpack.getByStrings("let{userId", ",{variant", '"primary",', { searchExports: true});
-
-    return <MessageButtonLarge autoFocus={autoFocus} onClose={() => PopUtils.popAll()} userId={userId} />
+function getIntlString(hash, parameter) {
+    if (parameter) return intl.intl.formatToPlainString(intl.t[`${hash}`], parameter);
+    return intl.intl.formatToPlainString(intl.t[`${hash}`]);
 }
-function MessageButtonSmallComponent({ onClose, userId, variant }) {
-    MessageButtonSmall ??= Webpack.getByStrings("userId", "tooltipText:", ",variant", "{text", { searchExports: true });
 
-    return <MessageButtonSmall onClose={() => PopUtils.popAll()} userId={userId} variant={variant} />
-}
-function FriendsButtonComponent({ relationshipType, shouldShowTooltip, type, themeColor, user }) {
-    FriendsButton ??= Webpack.getByStrings('menuItems', 'relationshipType', { searchExports: true });
-
-    return <FriendsButton relationshipType={relationshipType} shouldShowTooltip={shouldShowTooltip} type={type} themeColor={themeColor} user={user} />
-}
-function MoreOverflowButtonComponent({ user }) {
-    MoreOverflowButton ??= Webpack.getMangled('user-profile-overflow-menu', {
-        Button: Webpack.Filters.byStrings('popoutTargetRef')
-    });
-
-    return <MoreOverflowButton.Button user={user} />
-}
-function FriendAddButtonComponent({ autoFocus, userId, variant }) {
-    FriendAddButton ??= Webpack.getMangled('SEND_FRIEND_REQUEST,icon', {
-        AddFriend: Webpack.Filters.combine(Webpack.Filters.byStrings('{userId:'), Webpack.Filters.not(Webpack.Filters.byStrings('tooltipText')))
-    });
-
-    return <FriendAddButton.AddFriend autoFocus={autoFocus} userId={userId} variant={variant} />
-}
-function EditProfileButtonComponent({ user }) {
-    EditProfileButton ??= Webpack.getByStrings('trackUserProfileAction', 'EDIT_PROFILE', { searchExports: true });
-
-    return <EditProfileButton user={user} />
-}
-function BotAddButtonComponent({ user }) {
-    BotAddButton ??= Webpack.getByStrings('"user-bot-profile-add-app"');
-
-    return <BotAddButton user={user} />
-}
-function MarkdownComponent({ userBio }) {
-    MarkdownFormat ??= Webpack.getByStrings('userBio', 'disableAnimations');
-
-    return <MarkdownFormat className="userBio" userBio={userBio} />
-}
-function NoteComponent({ userId }) {
-    NoteRenderer ??= Webpack.getByStrings('hidePersonalInformation', 'onUpdate', 'placeholder');
-
-    return <NoteRenderer className="note" userId={userId} />
-}
-function ConnectionComponent({ connectedAccount, userId }) {
-    ConnectionRenderer ??= Webpack.getByStrings('connectedAccount', 'connectedAccountOpenIcon', 'CONNECTED_ACCOUNT_VIEWED', { searchExports: true });
-
-    return <ConnectionRenderer className="connectedAccount" connectedAccount={connectedAccount} userId={userId} showMetadata={false} />
-}
-function BoardEditRenderer({ user }) {
-    Board ??= Webpack.getByStrings('data-scroller', 'fade:!0,', {searchExports: true});
-
-    return <Board user={user} />
-}
-function RolePermissionHook({ guildId }) {
-    RolePermissionCheck ??= Webpack.getByStrings('.ADMINISTRATOR', '.MANAGE_MESSAGES');
-
-    return RolePermissionCheck({guildId});
-}
-function WidgetTagRenderer({ tags, widgetType, className }) {
-    TagRenderer ??= Webpack.getBySource('"EXPAND_GAME_TAGS"', {declarationFilter: x => String(x).includes('"EXPAND_GAME_TAGS"')});
-
-    return <TagRenderer tags={tags} widgetType={widgetType} className={className} />
-}
-function MutualFriendRenderer({user, status, guildId, onSelect}) {
-    MutualFriends ??= Webpack.getByStrings('MODAL_V2', 'discriminatorClass');
-
-    return <MutualFriends user={user} status={status} guildId={guildId} onSelect={onSelect} />
-}
+export function MessageButtonLargeComponent({ autoFocus, onClose, userId }) {
+    MessageButtonLarge ??= Webpack.getByStrings(
+        "let{userId", 
+        ",{variant", 
+        '"primary",', 
+        { searchExports: true }
+    );
     
-function MutualServerRenderer({key, user, guild, nick, onSelect}) {
-    MutualServers ??= Webpack.getByStrings('hasAvatarForGuild', 'nick');
-
-    return <MutualServers key={key} user={user} status={status} guild={guild} nick={nick} onSelect={onSelect} />
+    return React.createElement(MessageButtonLarge, { 
+        autoFocus, 
+        onClose: () => PopUtils.popAll(), 
+        userId, 
+        text: getIntlString("YzpScd") 
+    });
 }
 
-export {
-    MessageButtonLargeComponent,
-    MessageButtonSmallComponent,
-    FriendsButtonComponent,
-    MoreOverflowButtonComponent,
-    FriendAddButtonComponent,
-    EditProfileButtonComponent,
-    BotAddButtonComponent,
-    MarkdownComponent,
-    NoteComponent,
-    ConnectionComponent,
-    BoardEditRenderer,
-    RolePermissionHook,
-    WidgetTagRenderer,
-    MutualFriendRenderer,
-    MutualServerRenderer,
+export function MessageButtonSmallComponent({ onClose, userId, variant }) {
+    MessageButtonSmall ??= Webpack.getByStrings(
+        "userId", 
+        "tooltipText:", 
+        ",variant", 
+        "{text", 
+        { searchExports: true }
+    );
+    
+    return React.createElement(MessageButtonSmall, { 
+        onClose: () => PopUtils.popAll(), 
+        userId, 
+        variant 
+    });
 }
+
+export function MoreOverflowButtonComponent({ user }) {
+    MoreOverflowButton ??= Webpack.getMangled("user-profile-overflow-menu", {
+        Button: Webpack.Filters.byStrings("popoutTargetRef")
+    });
+    
+    return React.createElement(MoreOverflowButton.Button, { user });
+}
+
+export function SendFriendRequestButtonComponent({ autoFocus, userId, variant }) {
+    SendFriendRequestButton ??= Webpack.getMangled("SEND_FRIEND_REQUEST,icon", {
+        SendFriendRequest: Webpack.Filters.combine(
+            Webpack.Filters.byStrings("{userId:"), 
+            Webpack.Filters.not(Webpack.Filters.byStrings("tooltipText"))
+        )
+    });
+    
+    return React.createElement(SendFriendRequestButton.SendFriendRequest, { 
+        autoFocus, 
+        userId, 
+        variant, 
+        text: getIntlString("gc9aSx") 
+    });
+}
+
+export function PendingRequestButton() {
+    return React.createElement(
+        "button",
+        {
+            className: `${ButtonClasses.button} ${ButtonClasses.sm} ${ButtonClasses.secondary} ${ButtonClasses.hasText}`,
+            type: "button",
+            disabled: true,
+            style: { opacity: 0.6, cursor: "default" }
+        },
+        React.createElement("div", { className: `${ButtonClasses.buttonChildrenWrapper}` },
+            React.createElement("div", { 
+                className: `${ButtonClasses.buttonChildren}`, 
+                style: { fontSize: "14px", fontWeight: "500" } 
+            },
+                getIntlString("xMH6vD") // "Pending Request"
+            )
+        )
+    );
+}
+
+export function EditProfileButtonComponent({ user }) {
+    EditProfileButton ??= Webpack.getByStrings(
+        "trackUserProfileAction", 
+        "EDIT_PROFILE", 
+        { searchExports: true }
+    );
+    
+    return React.createElement(EditProfileButton, { user });
+}
+
+export function BotAddButtonComponent({ user }) {
+    BotAddButton ??= Webpack.getByStrings('"user-bot-profile-add-app"');
+    
+    return React.createElement(BotAddButton, { 
+        user, 
+        text: getIntlString("E64YCz") 
+    });
+}
+
+export function MarkdownComponent({ userBio }) {
+    MarkdownFormat ??= Webpack.getByStrings("userBio", "disableAnimations");
+    
+    return React.createElement(MarkdownFormat, { 
+        className: "userBio", 
+        userBio 
+    });
+}
+
+export function NoteComponent({ userId }) {
+    NoteRenderer ??= Webpack.getByStrings(
+        "hidePersonalInformation", 
+        "onUpdate", 
+        "placeholder"
+    );
+    
+    return React.createElement(NoteRenderer, { 
+        className: "note", 
+        userId 
+    });
+}
+
+export function ConnectionComponent({ connectedAccount, userId }) {
+    ConnectionRenderer ??= Webpack.getByStrings(
+        "connectedAccount", 
+        "connectedAccountOpenIcon", 
+        "CONNECTED_ACCOUNT_VIEWED", 
+        { searchExports: true }
+    );
+    
+    return React.createElement(ConnectionRenderer, { 
+        className: "connectedAccount", 
+        connectedAccount, 
+        userId, 
+        showMetadata: false 
+    });
+}
+
+export function BoardEditRenderer({ user }) {
+    Board ??= Webpack.getByStrings(
+        "data-scroller", 
+        "fade:!0,", 
+        { searchExports: true }
+    );
+    
+    return React.createElement(Board, { user });
+}
+
+export function RolePermissionHook({ guildId }) {
+    RolePermissionCheck ??= Webpack.getByStrings(
+        ".ADMINISTRATOR", 
+        ".MANAGE_MESSAGES"
+    );
+    
+    return RolePermissionCheck({ guildId });
+}
+
+export function WidgetTagRenderer({ tags, widgetType, className }) {
+    TagRenderer ??= Webpack.getBySource('"EXPAND_GAME_TAGS"', { 
+        declarationFilter: (x) => String(x).includes('"EXPAND_GAME_TAGS"') 
+    });
+    
+    return React.createElement(TagRenderer, { tags, widgetType, className });
+}
+
+export function MutualFriendRenderer({ user, status, guildId, onSelect }) {
+    MutualFriends ??= Webpack.getByStrings("MODAL_V2", "discriminatorClass");
+    
+    return React.createElement(MutualFriends, { 
+        user, 
+        status, 
+        guildId, 
+        onSelect 
+    });
+}
+
+export function MutualServerRenderer({ key, user, guild, nick, onSelect }) {
+    MutualServers ??= Webpack.getByStrings("hasAvatarForGuild", "nick");
+    return React.createElement(MutualServers, { 
+        key, 
+        user, 
+        guild, 
+        nick, 
+        onSelect 
+    });
+}
+
+export const Components = {
+    MessageButtonLarge: MessageButtonLargeComponent,
+    MessageButtonSmall: MessageButtonSmallComponent,
+    MoreOverflowButton: MoreOverflowButtonComponent,
+    SendFriendRequestButton: SendFriendRequestButtonComponent,
+    PendingRequestButton: PendingRequestButton,
+    EditProfileButton: EditProfileButtonComponent,
+    BotAddButton: BotAddButtonComponent,
+    Markdown: MarkdownComponent,
+    Note: NoteComponent,
+    Connection: ConnectionComponent,
+    BoardEdit: BoardEditRenderer,
+    RolePermission: RolePermissionHook,
+    WidgetTag: WidgetTagRenderer,
+    MutualFriend: MutualFriendRenderer,
+    MutualServer: MutualServerRenderer,
+};
+
+export default Components;
